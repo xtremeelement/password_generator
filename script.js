41c300733a176610870f8a20af8ab9a7289940d0
Brandon Downhour <xtremeelement@gmail.com>
2019-11-25 16:20:16 -0500

    added event listener function instead of onclick function and added red css change on copy to clipboard

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ var yourPassword = "";
 var numbers;
 var reset = "";
 
-// Function prompst user for password length
-function question() {
+//Function prompts user for password length and parameters with eventlistener
+document.querySelector(".button1").addEventListener("click", () => {
   charSet = "";
   numbers = prompt("How long do you want your password?");
   if (numbers < 8) {
@@ -32,7 +32,33 @@ function question() {
     }
   }
   passwordGenerator(numbers);
-}
+});
+// Function prompts user for password length and parameters with onclick in html tag
+// function question() {
+//   charSet = "";
+//   numbers = prompt("How long do you want your password?");
+//   if (numbers < 8) {
+//     alert("Password must be greater than 8 characters");
+//   } else {
+//     var caps = confirm("Use uppercase characters?");
+//     if (caps) {
+//       charSet += upperCase;
+//     }
+//     var lower = confirm("Use lowercase characters?");
+//     if (lower) {
+//       charSet += lowerCase;
+//     }
+//     var special = confirm("Use special characters?");
+//     if (special) {
+//       charSet += specialChars;
+//     }
+//     var nums = confirm("Use numbers?");
+//     if (nums) {
+//       charSet += numbersSet;
+//     }
+//   }
+//   passwordGenerator(numbers);
+// }
 //Calls the function that generates the password and disiplays
 
 //This function generates a random password and displays it.
@@ -42,6 +68,7 @@ function passwordGenerator(length) {
     yourPassword += charSet.charAt(Math.floor(Math.random() * charSet.length));
     length--;
     document.querySelector(".yourPass").textContent = yourPassword;
+    document.querySelector(".button2").style.backgroundColor = "red";
   }
   //Resets for next random generated password
   yourPassword = "";
