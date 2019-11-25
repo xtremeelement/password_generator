b3b1b3560393d02fb9720b0a899c4801dd73f528
Brandon Downhour <xtremeelement@gmail.com>
2019-11-25 13:29:57 -0500

    condensed code and added more comments

--- script.js
+++ script.js
@@ -5,16 +5,27 @@ var numbers;
 
 // Function prompst user for password length
 function question() {
-  numbers = prompt("How long do you want your passowrd?"); 
-    //Calls the function that generates the password and disiplays
-  passwordGenerator(numbers);  
+  numbers = prompt("How long do you want your passowrd?");
+  //Calls the function that generates the password and disiplays
+  passwordGenerator(numbers);
 }
 
 //This function generates a random password and displays it.
 function passwordGenerator(length) {
-  
   while (length > 0) {
-    yourPassword += charSet.charAt(Math.floor(Math.random() * charSet.length));    
-    length--;    
-    document.querySelector(".yourPass").textContent = yourPassword;    
+    yourPassword += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    length--;
+    document.querySelector(".yourPass").textContent = yourPassword;
   }
+}
+
+function copyclipboard() {
+  console.log("test");
+
+  var copyText = document.querySelector(".yourPass");
+
+  copyText.select();
+
+  document.execCommand("copy");
+  alert(`Copied the text: ${copyText.value}`);
+}
