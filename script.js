6aba9bb4a4e17990682fd153ad9f0e080d809b96
Brandon Downhour <xtremeelement@gmail.com>
2019-11-25 13:12:07 -0500

    removed unused code

--- script.js
+++ script.js
@@ -5,26 +5,20 @@ var numbers;
 
 function question() {
   numbers = prompt("How long do you want your passowrd?");
-  //   console.log(numbers);
+ 
 
   passwordGenerator(numbers);
 
-  //   document.querySelector(".yourPass").textContent = "test";
+  
 }
 
 function passwordGenerator(length) {
   console.log(charSet.length);
   while (length > 0) {
-    yourPassword += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    yourPassword += charSet.charAt(Math.floor(Math.random() * charSet.bit length));
     console.log(yourPassword);
     length--;
     console.log(length);
     document.querySelector(".yourPass").textContent = yourPassword;
-    // return yourPassword;
+    
   }
-
-  // for ( var i = 0; i < length; i++ ) {
-  //     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  //  }
-  //  return result;
-}
